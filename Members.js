8cc71f70061f494ba85b6a943300e2dfd18bb318
Moises Valdez <moisesvaldez075@gmail.com>
2020-03-04 20:48:32 -0800

    using express.js

---
+++ Members.js
@@ -0,0 +1,22 @@
+// Users
+const members = [{
+        id: 1,
+        name: 'moises valdez',
+        email: 'moises@email.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'John Doe',
+        email: 'john@email.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Mary Jane',
+        email: 'mary@email.com',
+        status: 'active'
+    }
+]
+
+module.exports = members;
---
+++ express.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const moment = require('moment');
+
+
+const app = express();
+
+// // middleware
+// const logger = (req, res, next) => {
+//     console.log(
+//         `${req.protocol}://${req.get('host')}${
+//             req.originalUrl
+//         }: ${moment().format()}`
+//     );
+//     next();
+// };
+
+// // init middleware
+// app.use(logger);
+
+// 
+
+
+// Set static pages
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// gets all members
+// app.get('/api/members', (req, res) => {
+//     res.json(members);
+// });
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}))
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Listing on port 5000'));
